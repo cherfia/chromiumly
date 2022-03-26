051a0c712ee1c82ee06acc6cb40500f621de5d5a
Taha Cherfia <taha.cherfia@gmail.com>
2022-03-26 22:21:20 +0100

    feat(common): add comments to types

--- src/common/converter.types.ts
+++ src/common/converter.types.ts
@@ -1,18 +1,21 @@
-type PageSize = { width: number; height: number };
+type PageSize = {
+  width: number; // Paper width, in inches (default 8.5)
+  height: number; //Paper height, in inches (default 11)
+};
 
 type PageMargins = {
-  top: number;
-  bottom: number;
-  left: number;
-  right: number;
+  top: number; // Top margin, in inches (default 0.39)
+  bottom: number; // Bottom margin, in inches (default 0.39)
+  left: number; // Left margin, in inches (default 0.39)
+  right: number; // Right margin, in inches (default 0.39)
 };
 
 export type PageProperties = {
   size?: PageSize;
   margins?: PageMargins;
-  preferCssPageSize?: boolean;
-  printBackground?: boolean;
-  landscape?: boolean;
-  scale?: number;
-  nativePageRanges?: { from: number; to: number };
+  preferCssPageSize?: boolean; // Define whether to prefer page size as defined by CSS (default false)
+  printBackground?: boolean; // Print the background graphics (default false)
+  landscape?: boolean; // Set the paper orientation to landscape (default false)
+  scale?: number; // The scale of the page rendering (default 1.0)
+  nativePageRanges?: { from: number; to: number }; // Page ranges to print
 };
