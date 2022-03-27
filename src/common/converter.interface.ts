7ec357fa9ccf0de80f6def68af4cff57f2d31025
Taha Cherfia <taha.cherfia@gmail.com>
2022-03-27 19:19:31 +0200

    chore(common): update convert() signature

--- src/common/converter.interface.ts
+++ src/common/converter.interface.ts
@@ -1,3 +1,11 @@
+import { PathLike } from "fs";
+
+import { PageProperties } from "./converter.types";
+
 export interface IConverter {
-  convert({ ...args }): Promise<Buffer> | Promise<void>;
+  convert({
+    ...args
+  }: {
+    [x: string]: string | PathLike | PageProperties;
+  }): Promise<Buffer> | Promise<void>;
 }
