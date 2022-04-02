1f571ff2d935549928aef2bfcf391872ddf0f0c9
Taha Cherfia <taha.cherfia@gmail.com>
2022-04-02 14:06:06 +0200

    feat: use typesctipt for jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,10 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "node",
   reporters: ["default", "jest-junit"],
   coverageDirectory: "./coverage/",
   collectCoverage: true,
 };
+export default config;
