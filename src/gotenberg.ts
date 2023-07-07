73320f35a7c224250f82dd6600dac6be1687ff25
Taha Abdelmoutaleb Cherfia <taha.cherfia@holi.team>
2023-07-07 19:35:03 +0200

    feat(config): load gotenberg endpoint from environment-specific file

--- src/gotenberg.ts
+++ src/gotenberg.ts
@@ -1,7 +1,19 @@
 process.env.SUPPRESS_NO_CONFIG_WARNING = "y";
 
-import "dotenv/config";
+import * as dotenv from "dotenv";
+import * as path from "path";
 import config from "config";
+
+// Load endpoint from environment-specific file (e.g., .env.development)
+const envFile = `.env.${process.env.NODE_ENV}`;
+const envFileFallback = ".env";
+
+const dotenvConfig = dotenv.config({ path: path.resolve(envFile) });
+
+// Fallback to loading the default environment file.
+if (dotenvConfig.error) {
+  dotenv.config({ path: path.resolve(envFileFallback) });
+}
 
 export class Gotenberg {
   public static endpoint: string =
