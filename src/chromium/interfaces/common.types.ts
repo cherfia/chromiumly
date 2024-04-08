696ee706afe1f271b2150dba738f79602fe861e3
Taha Abdelmoutaleb Cherfia <taha.cherfia@holi.team>
2024-04-08 14:25:29 +0200

    chore(chromium): fix comment typo

--- src/chromium/interfaces/common.types.ts
+++ src/chromium/interfaces/common.types.ts
@@ -6,7 +6,7 @@ export type ChromiumOptions = {
     header?: PathLikeOrReadStream;
     footer?: PathLikeOrReadStream;
     emulatedMediaType?: EmulatedMediaType;
-    waitDelay?: string; // Duration (e.g, '5s') to wait when loading an HTML document before convertion.
+    waitDelay?: string; // Duration (e.g, '5s') to wait when loading an HTML document before conversion.
     waitForExpression?: string; // JavaScript's expression to wait before converting an HTML document into PDF until it returns true.
     extraHttpHeaders?: Record<string, string>;
     failOnHttpStatusCodes?: number[]; // Return a 409 Conflict response if the HTTP status code is in the list (default [499,599])
