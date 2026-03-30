7697c21b91822f6a7ea21b245b7dff7e3b265a81
Taha Cherfia <taha.cherfia@gmail.com>
2026-03-30 12:38:06 +0200

    chore(deps): update dependency typescript to v6

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,7 +1,7 @@
 export { PdfFormat } from './constants';
 export { GotenbergUtils } from './gotenberg.utils';
 export { PdfEngineWatermarkStampUtils } from './pdf-engine-watermark-stamp.utils';
-export { PathLikeOrReadStream, Metadata } from './types';
+export type { PathLikeOrReadStream, Metadata } from './types';
 export type { PdfEngineRotate } from './types';
 export { appendPdfEngineRotate } from './pdf-engine-rotate.utils';
 export type {
